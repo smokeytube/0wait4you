c4665617c33cf58326f47e738942a275bfcd64de
smokeytube <67290867+smokeytube@users.noreply.github.com>
2020-12-26 01:02:26 -0500

    Finished everything. Should work perfectly

--- main.py
+++ main.py
@@ -20,7 +20,9 @@ def doubleclick():
 
 def processImage(image):
         print ("Processing image...")
-        return pyautogui.locateOnScreen(image, confidence=0.5)
+        start = pyautogui.locateOnScreen(image, confidence=0.8)
+        pyautogui.moveTo(start)
+        return start
 
 def click_image(image):
     start = pyautogui.locateOnScreen(image, confidence=0.8)
@@ -41,30 +43,51 @@ def follow(thefile):
             continue
         yield line
 
-# jointime = input("What time do you want to join? (24 hour time, HOUR:MINUTE ex. 22:37): ")
-# distag = input("What is your discord tag? (ex. @UserName#0000): ")
-# timeoutprompt = input("Timeout? (If you are AFK and do not want to enter 2b2t AFK) (y/n):")
-# soundwarning = input("Play an alarm when at a high queue position? (y/n): ")
-# warningpos = int(input("What position to warn you?: "))
-# if timeoutprompt == 'y' or timeoutprompt == 'yes':
-#     timeoutpos = int(input("What queue position to disconnect at?: "))
-#     recconect = input("Reconnect after disconnecting? (y/n): ")
-# else:
-#     timeoutpos = 69420 # 😎👉👉
-#     reconnect = 'y'
 
-jointime = ("04:00")
+jointime =  ("16:59")
 distag = ("<@773984010049028096>")
+diswebhook = ("Bruh get your own damn webhook")
 timeoutprompt = ('n')
 warningpos = 50
 timeoutprompt = 'y'
 soundwarning = 'n'
-if timeoutprompt == 'y' or timeoutprompt == 'yes':
-    timeoutpos = 5
-    reconnect = 'y'
-else:
-    timeoutpos = 69420 # 😎👉👉
-    reconnect = 'y'
+timeoutpos = 5
+reconnect = 'y'
+timesleep = 30
+
+with open('information.txt', 'r') as infer:
+    for inferreed in infer:
+        if "jointime: " in inferreed:
+            inferreedplit = inferreed.split('jointime: ')
+            jointime = inferreedplit[1].rstrip('\n')
+        if "distag: " in inferreed:
+            inferreedplit = inferreed.split('distag: ')
+            distag = inferreedplit[1].rstrip('\n')
+        if "diswebhook: " in inferreed:
+            inferreedplit = inferreed.split('diswebhook: ')
+            diswebhook = inferreedplit[1].rstrip('\n')
+        if "timeoutprompt: " in inferreed:
+            inferreedplit = inferreed.split('timeoutprompt: ')
+            timeoutprompt = inferreedplit[1].rstrip('\n')
+        if "soundwarning: " in inferreed:
+            inferreedplit = inferreed.split('soundwarning: ')
+            soundwarning = inferreedplit[1].rstrip('\n')
+        if "warningpos: " in inferreed:
+            inferreedplit = inferreed.split('warningpos: ')
+            warningpos = inferreedplit[1].rstrip('\n')
+        if "timeoutpos: " in inferreed:
+            inferreedplit = inferreed.split('timeoutpos: ')
+            timeoutpos = inferreedplit[1].rstrip('\n')
+        if "reconnect: " in inferreed:
+            inferreedplit = inferreed.split('reconnect: ')
+            reconnect = inferreedplit[1].rstrip('\n')
+        if "timesleep: " in inferreed:
+            inferreedplit = inferreed.split('timesleep: ')
+            reconnect = inferreedplit[1].rstrip('\n')
+
+warningpos = int(warningpos)
+timeoutpos = int(timeoutpos)
+timesleep = int(timesleep)
 
 repit = 1
 while True:
@@ -75,14 +98,18 @@ while True:
             logfile = open(os.getenv("APPDATA")+"/.minecraft/logs/latest.log", "r")
             loglines = follow(logfile)
             try:
-                webhook = DiscordWebhook(url='https://discord.com/api/webhooks/791062578259820554/iQf1YuL4QAPogIPiL6cVnjOafCUOQuT190WnMLaVqSGMLOoR63cjGarVTO0M0brctep9', content=("Started at " + current_time))
+                webhook = DiscordWebhook(url=diswebhook, content=("Started at " + current_time))
                 webhook.execute()
             except:
                 print ("Could not reach discord")
-            doubleclick_image('assets/2b2tlogo/2b2tlogoauto.png')
-            doubleclick_image('assets/2b2tlogo/2b2tlogolarge.png')
-            doubleclick_image('assets/2b2tlogo/2b2tlogonormal.png')
-            doubleclick_image('assets/2b2tlogo/2b2tlogosmall.png')
+            if processImage('assets/2b2tlogo/2b2tlogoauto.png'):
+                doubleclick_image('assets/2b2tlogo/2b2tlogoauto.png')
+            elif processImage('assets/2b2tlogo/2b2tlogolarge.png'):
+                doubleclick_image('assets/2b2tlogo/2b2tlogolarge.png')
+            elif processImage('assets/2b2tlogo/2b2tlogonormal.png'):
+                doubleclick_image('assets/2b2tlogo/2b2tlogonormal.png')
+            elif processImage('assets/2b2tlogo/2b2tlogosmall.png'):
+                doubleclick_image('assets/2b2tlogo/2b2tlogosmall.png')
             now = datetime.now()
             current_timeintlast = now.strftime("%H%M")
             current_timeintlast1 = int(current_timeintlast)
@@ -111,14 +138,15 @@ while True:
                         now = datetime.now()
                         current_timeintlast = now.strftime("%H%M")
                         current_timeintlast1 = int(current_timeintlast)
-                        time.sleep(30)
-                        if reconnect == 'n' or 'no':
+                        time.sleep(timesleep)
+                        if reconnect == 'n' or reconnect == 'no':
                             while True:
                                 time.sleep(69420)
-                        break
+                        else:
+                            break
                     elif queuepos1 < warningpos:
                         try:
-                            webhook = DiscordWebhook(url='https://discord.com/api/webhooks/791062578259820554/iQf1YuL4QAPogIPiL6cVnjOafCUOQuT190WnMLaVqSGMLOoR63cjGarVTO0M0brctep9', content=linechat[1] + ' ' + distag)
+                            webhook = DiscordWebhook(url=diswebhook, content=linechat[1] + ' ' + distag)
                             webhook.execute()
                         except:
                             print ("Could not reach discord")
@@ -131,7 +159,7 @@ while True:
                         current_timeintlast1 = int(current_timeintlast)
                     elif ("Position in queue") in linechat[1] or ("2b2t is full") in linechat[1]:
                         try:
-                            webhook = DiscordWebhook(url='https://discord.com/api/webhooks/791062578259820554/iQf1YuL4QAPogIPiL6cVnjOafCUOQuT190WnMLaVqSGMLOoR63cjGarVTO0M0brctep9', content=linechat[1])
+                            webhook = DiscordWebhook(url=diswebhook, content=linechat[1])
                             webhook.execute()
                         except:
                             print ("Could not reach discord")
@@ -143,19 +171,39 @@ while True:
                 else:
                     if processImage('assets/backtoserverlist/btslauto.png'):
                         click_image('assets/backtoserverlist/btslauto.png')
-                        time.sleep(5)
+                        try:
+                            webhook = DiscordWebhook(url=diswebhook, content=("Uh oh! Something went wrong. Attempting to reconnect..."))
+                            webhook.execute()
+                        except:
+                            pass
+                        time.sleep(timesleep)
                         break
                     elif processImage('assets/backtoserverlist/btsllarge.png'):
                         click_image('assets/backtoserverlist/btsllarge.png')
-                        time.sleep(5)
+                        try:
+                            webhook = DiscordWebhook(url=diswebhook, content=("Uh oh! Something went wrong. Attempting to reconnect..."))
+                            webhook.execute()
+                        except:
+                            pass
+                        time.sleep(timesleep)
                         break
                     elif processImage('assets/backtoserverlist/btslnormal.png'):
                         click_image('assets/backtoserverlist/btslnormal.png')
-                        time.sleep(5)
+                        try:
+                            webhook = DiscordWebhook(url=diswebhook, content=("Uh oh! Something went wrong. Attempting to reconnect..."))
+                            webhook.execute()
+                        except:
+                            pass
+                        time.sleep(timesleep)
                         break
                     elif processImage('assets/backtoserverlist/btslsmall.png'):
                         click_image('assets/backtoserverlist/btslsmall.png')
-                        time.sleep(5)
+                        try:
+                            webhook = DiscordWebhook(url=diswebhook, content=("Uh oh! Something went wrong. Attempting to reconnect..."))
+                            webhook.execute()
+                        except:
+                            pass
+                        time.sleep(timesleep)
                         break
                     else:
                         pass
--- testing.py
+++
@@ -1,5 +0,0 @@
-import random
-
-while True:
-    randb = random.randint(1,5)
-    print (randb)
